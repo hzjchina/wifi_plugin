f1890140196f6c0ae937c1f86fee52d77c80b534
win11 <hzjchina@yeah.net>
2022-09-28 19:00:22 +0800

    first commit

--- android/src/main/java/org/pcgy/wifi/wifi_plugin/WifiPlugin.java
+++ android/src/main/java/org/pcgy/wifi/wifi_plugin/WifiPlugin.java
@@ -1,48 +1,67 @@
 package org.pcgy.wifi.wifi_plugin;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import java.util.List;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** WifiPlugin */
-public class WifiPlugin implements FlutterPlugin, MethodCallHandler {
+public class WifiPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.RequestPermissionsResultListener {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   private Context applicationContext;
-
+private Activity mActivity;
   private boolean is5G = false;
+  private static final int PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 87654333;
+  private Result resultCallBack = null;
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "wifi_plugin");
     channel.setMethodCallHandler(this);
     applicationContext = flutterPluginBinding.getApplicationContext();
+
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    resultCallBack = result;
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     }else if(call.method.equals("wifiName")){
 
-      String wifiName = getWifiName();
-      result.success(wifiName);
-
+     if((Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)){
+       if (applicationContext.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+         setLocationData();
+       }else{
+         result.success(getWifiName());
+       }
+     }else{
+       result.success(getWifiName());
+     }
     } else {
       result.notImplemented();
     }
@@ -54,6 +73,14 @@ public class WifiPlugin implements FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(null);
   }
 
+  @RequiresApi(api = Build.VERSION_CODES.M)
+  private void setLocationData(){
+    LocationManager lm = (LocationManager) applicationContext.getSystemService(Context.LOCATION_SERVICE);
+    if(lm != null){
+      mActivity.requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION);
+    }
+
+  }
 
   private String getWifiName() {
     String mSsid = "";
@@ -83,16 +110,55 @@ public class WifiPlugin implements FlutterPlugin, MethodCallHandler {
         int frequence = info.getFrequency();
         if (frequence > 4900 && frequence < 5900) {
           // Connected 5G wifi. Device does not support 5G
-          //  mConfirmBtn.setTag(Boolean.TRUE);
-          //判断WiFi
-          //判断WiFi 不支持5G  选择另外的WiFi
           is5G = true;
-
         } else {
           is5G = false;
         }
       }
     }
     return mSsid;
+  }
+
+  // initialize members of this class with Activity
+  private void initActivity(Activity activity) {
+    mActivity = activity;
+  }
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+   // binding.addActivityResultListener(this);
+    initActivity(binding.getActivity());
+    binding.addRequestPermissionsResultListener(this);
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    mActivity = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+    initActivity(binding.getActivity());
+    binding.addRequestPermissionsResultListener(this);
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+      mActivity = null;
+  }
+
+  @Override
+  public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    final boolean wasPermissionGranted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+    switch (requestCode){
+      case PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION:
+        if (wasPermissionGranted) {
+          resultCallBack.success(getWifiName());
+        } else {
+//          resultCallBack.error( "Permission", "Fine location permission denied", null);
+          resultCallBack.success("");
+        }
+        return true;
+    }
+    return false;
   }
 }
